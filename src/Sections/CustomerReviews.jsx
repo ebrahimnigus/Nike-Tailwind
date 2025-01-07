e43b8ccb9be07300237254e3b9de88d7472a9a20
ebrahimnigus <ebrahimnigus9@gmail.com>
2025-01-07 14:08:44 +0300

    add Subscribe

--- src/Sections/CustomerReviews.jsx
+++ src/Sections/CustomerReviews.jsx
@@ -6,7 +6,7 @@ const CustomerReviews = () => {
     <section>
       <h3 className="font-palanquin text-center text-4xl font-bold">
         What Our
-        <span className="text-coral-red">Customers</span>
+        <span className="text-coral-red"> Customers</span>
         Say?
       </h3>
       <p className="info-text m-auto mt-4 max-w-lg text-center">Hear genuine stories from our satisfied customers about their exceptional experiences with us.</p>
