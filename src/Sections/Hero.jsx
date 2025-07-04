be7936fbf82fb4d8f62ede335eadbfefd68c75be
Ebrahim Ngus <ebrahimnigus9@gmail.com>
2025-07-05 00:24:12 +0300

    Update Hero.jsx

--- src/Sections/Hero.jsx
+++ src/Sections/Hero.jsx
@@ -33,7 +33,7 @@ const Hero = () => {
         height={1000}
         className="object-contain relative z-10"
         />
-        <div className="flex sm:gap-6 gap-5 absolute -bottom-[5%] sm:left-[10%] max-sm:px-6">
+        <div className="flex sm:gap-6 gap-5 absolute -bottom-[5%] sm:left-[10%] max-sm:px-6 mr-4">
           {shoes.map((shoe)=>(
             <div key={shoe} >
               <ShoeCard imgURL={shoe} changeBigShoeImage={(shoe)=> setBigShoeImg(shoe)} bigShoeImg={bigShoeImg}/>
